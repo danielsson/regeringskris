fe69fbcb0e60960b551c54bb7bbc8cb8d2d935ff
Mattias Danielsson <matdanie@kth.se>
2015-01-11 14:11:18 +0100

    Command handling by member function pointers.

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,34 +1,60 @@
 #include <iostream>
 #import "GameEngine.h"
 #import <memory>
-
+#import <fstream>
 
 void kris::GameEngine::run() {
+    std::string cmd;
+
+    print_welcome();
 
     while (isRunning) {
-
-        std::string cmd;
+        std::cout << environment->name() << ":> ";
         std::getline(std::cin, cmd);
 
-        if(cmd.size() < 2) {
-            requestEnd();
-            continue;
-        }
-
-        if (cmd == "describe") {
-            std::cout << environment->description() << std::endl;
+        if(router.find(cmd) != router.end()) {
+            (this->*router[cmd])(cmd);
+        } else {
+            std::cout << "Ingen visste vad du menade, så det skickades på remiss till KU efter att någun funnit det kränkande." << std::endl;
         }
     }
 }
 
-void kris::GameEngine::requestEnd() {
+void kris::GameEngine::print_welcome() {
+    std::__1::string line;
+    std::__1::ifstream w_file ("intro.txt");
+    if (w_file.is_open()) {
+        while (getline(w_file, line)) {
+            std::__1::cout << line << std::__1::endl;
+        }
+
+        w_file.close();
+    } else {
+        std::__1::cout << "File not found: intro.txt" << std::__1::endl;
+    }
+}
+
+void kris::GameEngine::requestEnd(std::string const & d) {
 
     isRunning = false;
 }
 
+
+void kris::GameEngine::cmd_describe(std::string const &) {
+    std::cout << environment->description() << std::endl;
+}
+
+
+void kris::GameEngine::init_router() {
+    router["describe"] = &GameEngine::cmd_describe;
+    router["quit"] = &GameEngine::requestEnd;
+}
+
 kris::GameEngine::GameEngine() {
 
-    hero = new entities::Hero("Merlin");
+    init_router();
+
+    hero = new entities::Hero("Löfven");
 
     entities::GenericItem* doll = new entities::GenericItem("Barbie", "A small doll");
     entities::GenericItem* apple = new entities::GenericItem("Apple", "An edible fruit");
@@ -36,4 +62,20 @@ kris::GameEngine::GameEngine() {
     environment = new env::GenericRoom("Stadsminsterns kontor", "Ett kontor mycket passande för en lägre mellanchef på ett halvstatligt pappersbruk.");
     environment->getItems().add_item(doll);
     environment->getItems().add_item(apple);
+}
+
+void kris::GameEngine::cmd_go(std::string const &aConst) {
+
+}
+
+void kris::GameEngine::cmd_give(std::string const &aConst) {
+
+}
+
+void kris::GameEngine::cmd_steal(std::string const &aConst) {
+
+}
+
+void kris::GameEngine::cmd_use(std::string const &aConst) {
+
 }
--- GameEngine.h
+++ GameEngine.h
@@ -1,6 +1,7 @@
 #import "world/entity.h"
 #import "world/env.h"
 #import <iostream>
+#include <map>
 
 #ifndef GAME_ENGINE_GUARD
 #define GAME_ENGINE_GUARD
@@ -8,17 +9,36 @@
 
 namespace kris {
 
+    class GameEngine;
+
+    typedef void (GameEngine::*CommandDealer)(std::string const&);
 
     class GameEngine {
 
     private:
+
         bool isRunning = true;
         env::Environment* environment;
         entities::Hero* hero;
+
+        std::map<std::string, CommandDealer> router;
+
+        void init_router();
+        void print_welcome();
+
     public:
         GameEngine();
         void run();
-        void requestEnd();
+        void requestEnd(std::string const &);
+
+
+        void cmd_describe(std::string const&);
+        void cmd_go(std::string const &);
+        void cmd_give(std::string const &);
+        void cmd_steal(std::string const &);
+        void cmd_use(std::string const &);
+
+
     };
 
 }
